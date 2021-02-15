7c3a5adb83a2c830f6caae4841a577b7bf78e1da
liuyaming <sarming@126.com>
2021-02-15 23:29:00 +0800

    登录成功页头显示用户昵称

--- xcEdu_service/xc-service-api/src/main/java/com/xuecheng/api/auth/AuthControllerApi.java
+++ xcEdu_service/xc-service-api/src/main/java/com/xuecheng/api/auth/AuthControllerApi.java
@@ -7,15 +7,28 @@ import com.xuecheng.framework.model.response.ResponseResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
-@Api(value = "用户认证",description = "用户认证接口")
+/**
+ * 1. 用户请求认证服务，登录成功
+ * 2。 用户登录成功，认证服务向cookie写入身份令牌，向redis写入user_token（身份令牌以及授权JWT授权令牌）
+ * 3。 客户端携带cookie中的身份令牌请求认证服务获取JWT
+ * 4。 认证服务根据身份令牌从redis中查询JWT令牌返回给客户端
+ * 5。 客户端拿到JWT获取客户信息，（用户登录成功在页头显示登录用户昵称）
+ */
+@Api(value = "用户认证", description = "用户认证接口")
 public interface AuthControllerApi {
-         
+
     @ApiOperation("登录")
     LoginResult login(LoginRequest loginRequest);
 
     @ApiOperation("退出")
     ResponseResult logout();
 
+    /**
+     * 1。 客户端携带cookie中的身份令牌请求认证服务获取JWT
+     * 2。 认证服务根据身份令牌从redis中查询JWT令牌返回给客户端
+     *
+     * @return
+     */
     @ApiOperation("查询user jwt令牌")
     JwtResult userjwt();
 
